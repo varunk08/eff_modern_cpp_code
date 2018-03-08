55d96969634f8103c1ece0d02115111f26c8d411
varun kumar <varunk08@users.noreply.github.com>
2018-03-08 09:53:00 -0500

    item 17 again

--- src/item_17.cpp
+++ src/item_17.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Widget
 {
 
-public:  
+public:
   // default copy constructor
   Widget(Widget& w) = default;
 
@@ -20,6 +20,14 @@ public:
 
   // default move assignment
   Widget& operator=(Widget&& rhs) = default;
+
+  // construct widget from anything
+  template<typename T>
+  Widget(const T& rhs);
+
+  // copy widget from anything
+  template<typename T>
+  Widget& operator=(const T& rhs);
 };
 
 int main(int argc, char** argv)
